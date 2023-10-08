f6f7e3310a103ef0842bfaf3d76fc9eb849888b9
very-sad-doggo <ryadovoyva@gmail.com>
2023-10-08 03:02:41 +0000

    scripts in progress

--- create_tables/create_tables.py
+++ create_tables/create_tables.py
@@ -8,5 +8,34 @@ conn = psycopg2.connect(f"host={DB_HOST} dbname={DB_NAME} user={DB_USER} passwor
 def create_mm_dept():
     cur = conn.cursor()
     cur.execute(
-            sql.SQL("")    
+            sql.SQL(    "CREATE TABLE mm.dept (" +
+                        "id serial PRIMARY KEY," +
+                        "name VARCHAR ( 50 ) NOT NULL," +
+                        "create_dt date," +
+                        "dates2 date)"
+  )    
+)
+    conn.commit()
+
+def insert_mm_dept():
+    cur = conn.cursor()
+    cur.execute(
+        sql.SQL(
+                "insert into mm.dept (id, name)" +
+                "values" +
+                "('1', 'Терапевтическое отделение')," +
+                "('2', 'Хирургическое отделение')," +
+                "('3', '2 кардиологическое (ОИМ)')," +
+                "('4', 'Приемное отделение')," +
+                "('5', 'Неврологическое отделение')"
+        )
+    )
+    conn.commit()
+
+def create_mm_tap():
+    cur = conn.cursor()
+    cur.execute(
+        sql.SQL(
+            
+        )
     )
